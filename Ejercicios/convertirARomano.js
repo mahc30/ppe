835eb51b568e0f38c1fe171c83290ab90f36a76c
Mahc30 <miguel.hincapiec@upb.edu.co>
2020-02-19 06:37:54 -0500

    Romanize

---
+++ Ejercicios/convertirARomano.js
@@ -0,0 +1,29 @@
+function Number(num) {
+
+    this.arabic = num;
+
+    var roman = {
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+
+    var str = '';
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(num / roman[i]);
+        num -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    this.roman = str;
+}
+
+var roman1 = new Number(89);
+console.log(roman1.roman)
